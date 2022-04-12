158dc24750cc2a7d9f38388d558e33a350d85373
Ivan Gabriel <gabriel.ivan@roweb.ro>
2022-04-12 20:52:05 +0300

    Create welcome screen

---
+++ InvoicesApp/Screens/Welcomescreen/Controller/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  InvoicesApp
+//
+//  Created by Ivan Gabriel on 12.04.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
